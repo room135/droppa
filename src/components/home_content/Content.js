d7ca1b8599f5fa2ffcdde1a47020811b38cc4235
rangsimant hanwongsa <hanwongsa.r@gmail.com>
2017-07-02 17:21:24 +0700

    push left menu category text list

--- src/components/home_content/Content.js
+++ src/components/home_content/Content.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import CategoryTextList from './CategoryTextList';
 
 class Content extends Component {
 	render() {
 		return (
 			<div className="Content container">
 				<h1>Hi Content</h1>
+				<CategoryTextList />
 			</div>
 		)
 	}
