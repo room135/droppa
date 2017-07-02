38cf865fcc54dbb9abbe505c2c1d9170725c565d
Thanadol Rewbumrung <thanadol.seo@gmail.com>
2017-07-02 18:05:49 +0700

    CatelogyIconList

--- src/components/home_content/Content.js
+++ src/components/home_content/Content.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import CategoryTextList from './CategoryTextList';
+import CategoryIconList from './CategoryIconList';
 
 class Content extends Component {
 	render() {
 		return (
 			<div className="Content container">
 				<h1>Hi Content</h1>
+				<CategoryIconList />
 				<CategoryTextList />
 			</div>
 		)
