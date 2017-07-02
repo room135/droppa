f15bff55dbaac50034e3d1527d9374a56661b0a0
rangsimant hanwongsa <hanwongsa.r@gmail.com>
2017-07-02 16:28:15 +0700

    create content component

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+	render() {
+		return (
+			<div className="Content container">
+				<h1>Hi Content</h1>
+			</div>
+		)
+	}
+}
+
+export default Content;
